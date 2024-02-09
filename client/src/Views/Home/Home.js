f1557b72c8acdf5b2c8dd50947699eecdb5395d6
shyam <shyamkorade0@gmail.com>
2024-02-09 21:46:43 +0530

    react app created

---
+++ client/src/Views/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+function Home(){
+    return(
+        <>
+            <Navbar/>
+            <div>
+                
+            </div>
+        </>
+    );
+}
+
+export default Home;
