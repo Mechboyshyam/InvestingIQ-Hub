43061618d5d87a601fdf5443b0093eedd9119076
shyam <shyamkorade0@gmail.com>
2024-02-13 14:31:27 +0530

    navbar added

--- client/src/Views/Home/Home.js
+++ client/src/Views/Home/Home.js
@@ -2,12 +2,11 @@ import React from "react";
 import Navbar from "../../Component/Navbar/Navbar";
 function Home(){
     return(
-        <>
-            <Navbar/>
+        <div> <Navbar/>
             <div>
-                
+                {/* 960472210000025 */}
             </div>
-        </>
+        </div>
     );
 }
 
