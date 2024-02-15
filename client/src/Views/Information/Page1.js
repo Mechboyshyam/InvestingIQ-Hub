d3c09d0ef9ddd0d472a99e6794bccce82123be79
shyam <shyamkorade0@gmail.com>
2024-02-15 14:09:13 +0530

    Navbar added and checked with text and going to work

--- client/src/Views/Information/Page1.js
+++ client/src/Views/Information/Page1.js
@@ -1,10 +1,11 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
+import Navbar from '../../Component/Navbar/Navbar';
 
 export class Page1 extends PureComponent {
   render() {
     return (
-      <div>
-        
+      <div><Navbar/> <hr />
+       <div className='text-center'> I think I'm visible to you!</div>
       </div>
     )
   }
