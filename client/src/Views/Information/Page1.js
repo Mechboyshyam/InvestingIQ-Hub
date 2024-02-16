14d8330ea123a7bd4f5582ad5d4da242fa12b804
shyam <shyamkorade0@gmail.com>
2024-02-16 11:18:12 +0530

    blog page design started

--- client/src/Views/Information/Page1.js
+++ client/src/Views/Information/Page1.js
@@ -1,11 +1,16 @@
 import React, { PureComponent } from 'react';
 import Navbar from '../../Component/Navbar/Navbar';
+import './Page1.css';
 
 export class Page1 extends PureComponent {
   render() {
     return (
       <div><Navbar/> <hr />
-       <div className='text-center'> I think I'm visible to you!</div>
+       <div className="container">
+        <h1 className="text-center title">
+          Here is my title... please read cerefully.
+        </h1>
+       </div>
       </div>
     )
   }
