92c3aa1ea27a41e9459a883fdf315a30da13cd15
shyam <shyamkorade0@gmail.com>
2024-02-18 15:54:51 +0530

    info added

--- client/src/Views/Information/Page1.js
+++ client/src/Views/Information/Page1.js
@@ -23,9 +23,9 @@ export class Page1 extends PureComponent {
                 Investing in Stock Market from basic to advanace."
               </h4>
               <p>
-                {'\t'} Do you ever have que in your mind that who can invest? Is there
-                any criteria for investing? How to invest? Few people say share
-                market is gambling, really? You will get all ans of the
+                {"\t"} Do you ever have que in your mind that who can invest? Is
+                there any criteria for investing? How to invest? Few people say
+                share market is gambling, really? You will get all ans of the
                 questions listed here and also the questions in your mind.
               </p>{" "}
               <hr />
@@ -55,6 +55,43 @@ export class Page1 extends PureComponent {
                 remember that there is selling and buying of vegetables and
                 fruits. In the same way it is also market place where buying and
                 selling of stocks occurs.
+              </p>
+              <p>
+              There is no restriction to invest, it can be small investor or big
+              mutual funds, FII and DII companies.
+              <ul>
+                <li>FII : Foreign Institutional Investor</li>
+                <li>DII : Domestic Institutional Investor</li>
+              </ul>
+              </p> <hr />
+
+              <span>
+                <b>Before investing you must have to know the benefits of these business:-</b>
+                <ol>
+                  <li>You will get more time, if you know very well about making profit. </li>
+                  <li>This business doesn't need proper place or office. You just need your Laptop or Mobile.</li>
+                </ol>
+              </span>
+              {/* what is nse and bse */}
+              <p>
+                <b>What is NSE and BSE?</b>
+                <ol>
+                  <li>
+                    NSE : It is a nifty index. It contains 50 big market cap stocks, also called as NIFTY-50.
+                  </li>
+                  <li>
+                    BSE : BSE is a Sensex. It contain 30 big market cap stocks.
+                  </li>
+                  <li>
+                    There are some more indexes in the indian stock market:-
+                    <ul>
+                      <li>Banknifty</li>
+                      <li>FinNifty</li>
+                      <li>MidcapNifty</li>
+                      <li>Bankex</li>
+                    </ul>
+                  </li>
+                </ol>
               </p>
             </div>
           </div>
