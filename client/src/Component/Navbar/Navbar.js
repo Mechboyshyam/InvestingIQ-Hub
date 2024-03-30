2b43b1c74410ab9c292e4dac941af995e2e590ae
shyam <shyamkorade0@gmail.com>
2024-03-30 13:36:18 +0530

    bootstrap 5 updated in navbar

--- client/src/Component/Navbar/Navbar.js
+++ client/src/Component/Navbar/Navbar.js
@@ -1,76 +1,92 @@
-import "./Navbar.css";
+import React from "react";
 import { Link } from "react-router-dom";
-
-export default  function Navbar() {
-  
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg navbar-dark sticky-top z-3 bg-teal ">
-          <span className="navbar-brand bold">ɪɴᴠᴇꜱᴇᴛɪɴɢɪQ-ʜᴜʙ</span>
-          {/* <span className="navbar-brand bold">𝐼𝓃𝓋𝑒𝓈𝑒𝓉𝒾𝓃𝑔𝐼𝒬-𝐻𝓊𝒷</span> */}
+import './Navbar.css';
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid nav-color">
+          <span className="navbar-brand ms-2 text-light fw-bold fs-3">
+            InvestingIQ-Hub
+          </span>
           <button
             className="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div
-            className="main_nav collapse navbar-collapse"
-            id="navbarSupportedContent"
-          >
-            <div className="col-4">
-              <ul className="nav_items navbar-nav mr-auto">
-                <li className="nav-item active btn bold">
-                <Link to="" className="text-decoration-none text-decoration btn font-size text-light">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item active bold btn">
-                <Link to="blogs" className="text-decoration-none text-decoration btn font-size text-light">
-                    Blogs
-                  </Link>
-                </li>
-                <li className="nav-item active btn bold">
-                <Link to="about" className="text-decoration-none text-decoration btn font-size text-light">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="" className="text-decoration-none text-light ms-3 fs-5 fw-semibold btn">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="blogs" className="text-decoration-none text-light ms-1 fs-5 fw-semibold btn">
+            Blogs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="about" className="text-decoration-none text-light ms-1 fs-5 fw-semibold btn">
                     About
                   </Link>
                 </li>
-                {/* <li className="nav-item active btn bold">
-                <a className="nav-link disabled" href="/">Analysis</a>
-              </li> */}
-
-                <li className="nav-item active btn bold">
-                  <Link to="contact" className="text-decoration-none text-decoration btn font-size text-light">
+                <li className="nav-item">
+                  <Link to="contact" className="text-decoration-none text-light ms-1 fs-5 fw-semibold btn">
                     Contact
                   </Link>
-
-                  {/* <a className="nav-link disabled" href="/contact">Contact</a> */}
                 </li>
-              </ul>
-            </div>
-            <div className="col-4">
-              {/* <span className="btn btn-outline-dark border-bold bold">
-              <DateTime></DateTime>
-              </span> */}
-            </div>
-            <div className="col-4 ">
-              <form className="form-inline search-box my-2 my-lg-0">
-                <input
-                  className="form-control mr-sm-2 text-center border-bold"
-                  type="search"
-                  placeholder="Search here..."
-                  aria-label="Search"
-                />
-              </form>
-            </div>
+              {/* <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li> */}
+            </ul>
+            <form className="" role="search">
+              <input
+                className="form-control me-2 text-center search-box"
+                type="search"
+                placeholder="Search here..."
+                aria-label="Search"
+              />
+            </form>
           </div>
-        </nav>
-      </>
-    );
-  
+        </div>
+      </nav>
+    </div>
+  );
 }
+
+export default Navbar;
---
+++ client/src/Component/Navbar/sample.js
@@ -0,0 +1,80 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+export default  function Navbar() {
+  
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark sticky-top z-3 bg-teal ">
+          <span className="navbar-brand bold">ɪɴᴠᴇꜱᴇᴛɪɴɢɪQ-ʜᴜʙ</span>
+          {/* <span className="navbar-brand bold">𝐼𝓃𝓋𝑒𝓈𝑒𝓉𝒾𝓃𝑔𝐼𝒬-𝐻𝓊𝒷</span> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="main_nav collapse navbar-collapse"
+            id="navbarSupportedContent"
+          >
+            <div className="col-4">
+              <ul className="nav_items navbar-nav mr-auto">
+                <li className="nav-item active btn bold">
+                <Link to="" className="text-decoration-none text-decoration btn font-size text-light">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item active bold btn">
+                <Link to="blogs" className="text-decoration-none text-decoration btn font-size text-light">
+                    Blogs
+                  </Link>
+                </li>
+                <li className="nav-item active btn bold">
+                <Link to="about" className="text-decoration-none text-decoration btn font-size text-light">
+                    About
+                  </Link>
+                </li>
+                {/* <li className="nav-item active btn bold">
+                <a className="nav-link disabled" href="/">Analysis</a>
+              </li> */}
+
+                <li className="nav-item active btn bold">
+                  <Link to="contact" className="text-decoration-none text-decoration btn font-size text-light">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-4">
+              {/* <span className="btn btn-outline-dark border-bold bold">
+              <DateTime></DateTime>
+              </span> */}
+            </div>
+            <div className="col-4 ">
+              <form className="form-inline search-box my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2 text-center border-bold"
+                  type="search"
+                  placeholder="Search the content"
+                  aria-label="Search"
+                />
+                {/* <button
+                className="contact hover-eff-btn bold my-2 my-sm-0"
+                type="submit"
+              >
+                <a href="/contact" className="text-decoration-none contact text-light">Contact</a>
+              </button> */}
+              </form>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  
+}
