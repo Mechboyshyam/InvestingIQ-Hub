a43cf1f4a8636d144ad217a72916ac8b066cf900
shyam <shyamkorade0@gmail.com>
2024-03-31 22:15:32 +0530

    trying to add read more and read less button

--- client/src/Component/BlogCard/Blogcard.js
+++ client/src/Component/BlogCard/Blogcard.js
@@ -4,29 +4,45 @@ import "./BlogCard.css";
 function BlogCard({ blogs }) {
   console.log("blogs", blogs);
 
+  const myFunction = () => {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  };
+
   return (
     <div className="container parent-border">
       <div className="row">
         <div className="col-12 mt-2">
-          {/* This is common title for blog */}
           <h3 className="text-center title-design">
             Welcome aboard our blog! We're delighted to have you join us as we
             kick off this adventure together. Get ready to dive into intriguing
             topics and insightful discussions. Let's make this journey one to
             remember!
           </h3>
-          {/* <hr /> */}
         </div>
       </div>
       <div className="row">
-        <div className="col-md-3  content p-3">
+        <div className="col-md-3 content p-3">
           <h2 className="text-center">
             <b>Content List</b> <br />
           </h2>
           {blogs?.map((item) => (
-            <h6 >
+            <h6>
               <ul>
-                <li><b>{item?.title}</b></li>
+                <li>
+                  <b>{item?.title}</b>
+                </li>
               </ul>
             </h6>
           ))}
@@ -35,19 +51,16 @@ function BlogCard({ blogs }) {
           {blogs?.map((item) => (
             <div key={item?._id} className="mb-4 blog-border">
               <div className="text-center">
-                {/* title of the blog */}
                 <h3 className="text-center title-design-topic">
                   {item?.title}
                 </h3>
-                <hr />
-                {/* image of that blog */}
                 <img
                   className="img-fluid"
                   src={item?.img}
                   alt="Blog Thumbnail"
                 />
               </div>
-              <hr />
+              <br />
               <div>
                 {item.topic.map((topic) => (
                   <div key={topic.title}>
@@ -63,25 +76,29 @@ function BlogCard({ blogs }) {
                 ))}
               </div>
               <div>
-                {/* title of array */}
-                {item.subList.map((subListItem, index) => (
-                  <div key={index}>
-                    <h4>{subListItem.subListTitle}</h4>
-                    <div>
-                      {/* here array is mapped */}
-                      <ol>
-                        {subListItem.subListContent.map(
-                          (subListContentItem, subIndex) => (
-                            <li key={subIndex}>
-                              {subListContentItem.subListItem}
-                            </li>
-                          )
-                        )}
-                      </ol>
+                <span id="dots"></span>
+                <span id="more">
+                  {item.subList.map((subListItem, index) => (
+                    <div key={index}>
+                      <h4>{subListItem.subListTitle}</h4>
+                      <div>
+                        <ol>
+                          {subListItem.subListContent.map(
+                            (subListContentItem, subIndex) => (
+                              <li key={subIndex}>
+                                {subListContentItem.subListItem}
+                              </li>
+                            )
+                          )}
+                        </ol>
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </span>
               </div>
+              <button onClick={myFunction} id="myBtn">
+                Read more
+              </button>
             </div>
           ))}
         </div>
