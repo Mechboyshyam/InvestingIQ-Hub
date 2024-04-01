fc907f5b8ee7b4655aae0d74e516e2d8414f25b5
shyam <shyamkorade0@gmail.com>
2024-04-01 16:42:13 +0530

    onclick function added so it will fetch that specific blog

--- client/src/Component/BlogCard/Blogcard.js
+++ client/src/Component/BlogCard/Blogcard.js
@@ -1,24 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./BlogCard.css";
 
 function BlogCard({ blogs }) {
-  console.log("blogs", blogs);
+  const [selectedContent, setSelectedContent] = useState(0); // Initialize with the index of the first blog
 
-  const myFunction = () => {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  const handleContentClick = (index) => {
+    setSelectedContent(index === selectedContent ? null : index);
   };
+
+  useEffect(() => {
+    // Ensure first blog is displayed on component mount
+    setSelectedContent(0);
+  }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
     <div className="container parent-border">
@@ -37,32 +30,32 @@ function BlogCard({ blogs }) {
           <h2 className="text-center">
             <b>Content List</b> <br />
           </h2>
-          {blogs?.map((item) => (
-            <h6>
+          {blogs?.map((item, index) => (
+            <h6 key={index} onClick={() => handleContentClick(index)} style={{ cursor: 'pointer', textDecoration: 'none', color: selectedContent === index ? 'blue' : 'black' }}>
               <ul>
                 <li>
-                  <b>{item?.title}</b>
+                  <b>{item.title}</b>
                 </li>
               </ul>
             </h6>
           ))}
         </div>
         <div className="col-md-9">
-          {blogs?.map((item) => (
-            <div key={item?._id} className="mb-4 blog-border">
+          {selectedContent !== null && (
+            <div className="mb-4 blog-border">
               <div className="text-center">
                 <h3 className="text-center title-design-topic">
-                  {item?.title}
+                  {blogs[selectedContent]?.title}
                 </h3>
                 <img
                   className="img-fluid"
-                  src={item?.img}
+                  src={blogs[selectedContent]?.img}
                   alt="Blog Thumbnail"
                 />
               </div>
               <br />
               <div>
-                {item.topic.map((topic) => (
+                {blogs[selectedContent]?.topic.map((topic) => (
                   <div key={topic.title}>
                     <h4>{topic.title}</h4>
                     <div>
@@ -76,31 +69,25 @@ function BlogCard({ blogs }) {
                 ))}
               </div>
               <div>
-                <span id="dots"></span>
-                <span id="more">
-                  {item.subList.map((subListItem, index) => (
-                    <div key={index}>
-                      <h4>{subListItem.subListTitle}</h4>
-                      <div>
-                        <ol>
-                          {subListItem.subListContent.map(
-                            (subListContentItem, subIndex) => (
-                              <li key={subIndex}>
-                                {subListContentItem.subListItem}
-                              </li>
-                            )
-                          )}
-                        </ol>
-                      </div>
+                {blogs[selectedContent]?.subList.map((subListItem, index) => (
+                  <div key={index}>
+                    <h4>{subListItem.subListTitle}</h4>
+                    <div>
+                      <ol>
+                        {subListItem.subListContent.map(
+                          (subListContentItem, subIndex) => (
+                            <li key={subIndex}>
+                              {subListContentItem.subListItem}
+                            </li>
+                          )
+                        )}
+                      </ol>
                     </div>
-                  ))}
-                </span>
+                  </div>
+                ))}
               </div>
-              <button onClick={myFunction} id="myBtn">
-                Read more
-              </button>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
